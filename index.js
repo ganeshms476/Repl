395e4b0bbd8cc3172e288452a3ca0e12e1e36a8d
ganeshms <>
2023-03-27 09:07:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// // let a = 10;
+// // let b = "10"
+// // console.log(a==b)
+
+// let a = {
+//   g:"sd",
+//     h:"sd",
+//     i:"sd",
+
+//     j:"sd"
+// }
+
+let k = [1,2,34,4,5]
+// k.splice(2,1,23,232)
+k.forEach((element)=>{
+  console.log(element)
+})
+
+
+// console.log(b.length)
+// console.log(b.slice(15))
